fea72f4b7b36cc74a9303be4911b81f01872a6f6
Nicat <bayramov.nicat@hotmail.com>
2025-02-06 05:55:03 +0400

    sphereGeometry

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,8 @@ import "./style.css";
 import { mat4 } from "gl-matrix";
 
 const viewer = createPanoramaViewer("app", "/panorama.jpeg");
-viewer.yaw = 90;
+viewer.yaw = 180;
+viewer.fov = 180;
 
 function createPanoramaViewer(elmId: string, url: string) {
   const container = document.getElementById(elmId)!;
@@ -10,24 +11,26 @@ function createPanoramaViewer(elmId: string, url: string) {
     throw Error("Container not found");
   }
 
-  // create canvas
-  const canvas = document.createElement("canvas");
-  const gl = initWebGlContext(canvas, {});
-
-  container.appendChild(canvas);
-
-  // get pixel ratio
-  const dpr = window.devicePixelRatio;
-
+  const DPR = window.devicePixelRatio;
+  const MIN_FOV = 10;
+  const MAX_FOV = 140;
   let width = 1;
   let height = 1;
+
+  // create canvas
+  const canvas = document.createElement("canvas");
+  const gl = initWebGlContext(canvas);
+  container.appendChild(canvas);
 
   resizeCanvas();
 
   const shaderProgram = initShaderProgram();
   const programInfo = getProgramInfo(shaderProgram);
-  const sphere = createSphere(128, 64);
-  const buffers = initBuffers(sphere);
+
+  const geometry = sphereGeometry(100, 128, 64);
+
+  const webglAttrs = webGLAttributes(gl);
+  const buffers = initBuffers(geometry);
   const texture = loadTexture(url);
 
   let isDragging = false;
@@ -48,8 +51,8 @@ function createPanoramaViewer(elmId: string, url: string) {
   function resizeCanvas() {
     width = container.clientWidth;
     height = container.clientHeight;
-    canvas.width = width * dpr;
-    canvas.height = height * dpr;
+    canvas.width = width * DPR;
+    canvas.height = height * DPR;
   }
 
   function initShaderProgram(): WebGLProgram {
@@ -137,38 +140,39 @@ function createPanoramaViewer(elmId: string, url: string) {
   }
 
   function initBuffers(sphere: {
-    positions: number[];
-    textureCoordinates: number[];
+    position: Float32Array;
+    normal?: Float32Array;
+    uv: Float32Array;
     indices: number[];
   }) {
-    const positionBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-    gl.bufferData(
-      gl.ARRAY_BUFFER,
-      new Float32Array(sphere.positions),
-      gl.STATIC_DRAW
+    const position = webglAttrs.update(
+      {
+        array: sphere.position,
+        usage: gl.STATIC_DRAW,
+      },
+      gl.ARRAY_BUFFER
     );
 
-    const textureCoordBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, textureCoordBuffer);
-    gl.bufferData(
-      gl.ARRAY_BUFFER,
-      new Float32Array(sphere.textureCoordinates),
-      gl.STATIC_DRAW
+    const textureCoord = webglAttrs.update(
+      {
+        array: sphere.uv,
+        usage: gl.STATIC_DRAW,
+      },
+      gl.ARRAY_BUFFER
     );
 
-    const indexBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
-    gl.bufferData(
-      gl.ELEMENT_ARRAY_BUFFER,
-      new Uint16Array(sphere.indices),
-      gl.STATIC_DRAW
+    const indices = webglAttrs.update(
+      {
+        array: new Uint16Array(sphere.indices),
+        usage: gl.STATIC_DRAW,
+      },
+      gl.ELEMENT_ARRAY_BUFFER
     );
 
     return {
-      position: positionBuffer,
-      textureCoord: textureCoordBuffer,
-      indices: indexBuffer,
+      position,
+      textureCoord,
+      indices,
     };
   }
 
@@ -269,7 +273,7 @@ function createPanoramaViewer(elmId: string, url: string) {
       e.preventDefault();
       const zoomSpeed = 0.1;
       fov += e.deltaY * zoomSpeed * (Math.PI / 180);
-      fov = clamp(fov, deg2rad(30), deg2rad(120));
+      fov = clamp(fov, deg2rad(MIN_FOV), deg2rad(MAX_FOV));
       drawScene();
     });
   }
@@ -331,7 +335,12 @@ function createPanoramaViewer(elmId: string, url: string) {
     gl.uniform1i(programInfo.uniformLocations.uSampler, 0);
 
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, buffers.indices);
-    gl.drawElements(gl.TRIANGLES, sphere.indexCount, gl.UNSIGNED_SHORT, 0);
+    gl.drawElements(
+      gl.TRIANGLES,
+      geometry.indices.length,
+      gl.UNSIGNED_SHORT,
+      0
+    );
   }
 
   return {
@@ -353,63 +362,9 @@ function createPanoramaViewer(elmId: string, url: string) {
       return rad2deg(fov);
     },
     set fov(value) {
-      fov = deg2rad(value);
+      fov = deg2rad(clamp(value, MIN_FOV, MAX_FOV));
       drawScene();
     },
-  };
-}
-
-function createSphere(latitudeBands: number, longitudeBands: number) {
-  // latitude bands must be power of 2
-  if (!isPowerOf2(latitudeBands)) {
-    throw new Error("latitudeBands must be power of 2");
-  }
-
-  // longitudeBands must be half of latitudeBands
-  if (longitudeBands !== latitudeBands / 2) {
-    throw new Error("longitudeBands must be half of latitudeBands");
-  }
-
-  const positions = [];
-  const textureCoordinates = [];
-  const indices = [];
-
-  for (let latNumber = 0; latNumber <= latitudeBands; latNumber++) {
-    const theta = (latNumber * Math.PI) / latitudeBands;
-    const sinTheta = Math.sin(theta);
-    const cosTheta = Math.cos(theta);
-
-    for (let longNumber = 0; longNumber <= longitudeBands; longNumber++) {
-      const phi = (longNumber * 2 * Math.PI) / longitudeBands;
-      const sinPhi = Math.sin(phi);
-      const cosPhi = Math.cos(phi);
-
-      const x = cosPhi * sinTheta;
-      const y = cosTheta;
-      const z = sinPhi * sinTheta;
-      const u = longNumber / longitudeBands;
-      const v = latNumber / latitudeBands;
-
-      positions.push(x, y, z);
-      textureCoordinates.push(u, v);
-    }
-  }
-
-  for (let latNumber = 0; latNumber < latitudeBands; latNumber++) {
-    for (let longNumber = 0; longNumber < longitudeBands; longNumber++) {
-      const first = latNumber * (longitudeBands + 1) + longNumber;
-      const second = first + longitudeBands + 1;
-
-      indices.push(first, second, first + 1);
-      indices.push(second, second + 1, first + 1);
-    }
-  }
-
-  return {
-    positions: positions,
-    textureCoordinates: textureCoordinates,
-    indices: indices,
-    indexCount: indices.length,
   };
 }
 
@@ -461,4 +416,318 @@ function initWebGlContext(
   }
 
   return gl;
+}
+
+// function sphereGeometry(latitudeBands: number, longitudeBands: number) {
+//   // latitude bands must be power of 2
+//   if (!isPowerOf2(latitudeBands)) {
+//     throw new Error("latitudeBands must be power of 2");
+//   }
+
+//   // longitudeBands must be half of latitudeBands
+//   if (longitudeBands !== latitudeBands / 2) {
+//     throw new Error("longitudeBands must be half of latitudeBands");
+//   }
+
+//   const positions = [];
+//   const textureCoordinates = [];
+//   const indices = [];
+
+//   for (let latNumber = 0; latNumber <= latitudeBands; latNumber++) {
+//     const theta = (latNumber * Math.PI) / latitudeBands;
+//     const sinTheta = Math.sin(theta);
+//     const cosTheta = Math.cos(theta);
+
+//     for (let longNumber = 0; longNumber <= longitudeBands; longNumber++) {
+//       const phi = (longNumber * 2 * Math.PI) / longitudeBands;
+//       const sinPhi = Math.sin(phi);
+//       const cosPhi = Math.cos(phi);
+
+//       const x = cosPhi * sinTheta;
+//       const y = cosTheta;
+//       const z = sinPhi * sinTheta;
+//       const u = longNumber / longitudeBands;
+//       const v = latNumber / latitudeBands;
+
+//       positions.push(x, y, z);
+//       textureCoordinates.push(u, v);
+//     }
+//   }
+
+//   for (let latNumber = 0; latNumber < latitudeBands; latNumber++) {
+//     for (let longNumber = 0; longNumber < longitudeBands; longNumber++) {
+//       const first = latNumber * (longitudeBands + 1) + longNumber;
+//       const second = first + longitudeBands + 1;
+
+//       indices.push(first, second, first + 1);
+//       indices.push(second, second + 1, first + 1);
+//     }
+//   }
+
+//   return {
+//     position: new Float32Array(positions),
+//     uv: new Float32Array(textureCoordinates),
+//     indices: indices,
+//   };
+// }
+
+function sphereGeometry(
+  radius = 1,
+  widthSegments = 32,
+  heightSegments = 16,
+  phiStart = 0,
+  phiLength = Math.PI * 2,
+  thetaStart = 0,
+  thetaLength = Math.PI
+) {
+  widthSegments = Math.max(3, Math.floor(widthSegments));
+  heightSegments = Math.max(2, Math.floor(heightSegments));
+
+  const thetaEnd = Math.min(thetaStart + thetaLength, Math.PI);
+
+  let index = 0;
+  const grid: number[][] = [];
+
+  const vertex = vector3();
+  const normal = vector3();
+
+  // buffers
+  const indices: number[] = [];
+  const vertices: number[] = [];
+  const normals: number[] = [];
+  const uvs: number[] = [];
+
+  // generate vertices, normals and uvs
+  for (let iy = 0; iy <= heightSegments; iy++) {
+    const verticesRow = [];
+
+    const v = iy / heightSegments;
+
+    // special case for the poles
+    let uOffset = 0;
+
+    if (iy === 0 && thetaStart === 0) {
+      uOffset = 0.5 / widthSegments;
+    } else if (iy === heightSegments && thetaEnd === Math.PI) {
+      uOffset = -0.5 / widthSegments;
+    }
+
+    for (let ix = 0; ix <= widthSegments; ix++) {
+      const u = ix / widthSegments;
+
+      // vertex
+      vertex.x =
+        -radius *
+        Math.cos(phiStart + u * phiLength) *
+        Math.sin(thetaStart + v * thetaLength);
+      vertex.y = radius * Math.cos(thetaStart + v * thetaLength);
+      vertex.z =
+        radius *
+        Math.sin(phiStart + u * phiLength) *
+        Math.sin(thetaStart + v * thetaLength);
+
+      vertices.push(vertex.x, vertex.y, vertex.z);
+
+      // normal
+      normal.copy(vertex).normalize();
+      normals.push(normal.x, normal.y, normal.z);
+
+      // uv
+      uvs.push(1 - u + uOffset, v);
+
+      verticesRow.push(index++);
+    }
+
+    grid.push(verticesRow);
+  }
+
+  // indices
+  for (let iy = 0; iy < heightSegments; iy++) {
+    for (let ix = 0; ix < widthSegments; ix++) {
+      const a = grid[iy][ix + 1];
+      const b = grid[iy][ix];
+      const c = grid[iy + 1][ix];
+      const d = grid[iy + 1][ix + 1];
+
+      if (iy !== 0 || thetaStart > 0) indices.push(a, b, d);
+      if (iy !== heightSegments - 1 || thetaEnd < Math.PI)
+        indices.push(b, c, d);
+    }
+  }
+
+  // build geometry
+
+  return {
+    indices,
+    position: new Float32Array(vertices),
+    normal: new Float32Array(normals),
+    uv: new Float32Array(uvs),
+  };
+}
+
+function vector3(x = 0, y = 0, z = 0) {
+  const res = {
+    x,
+    y,
+    z,
+    copy,
+    normalize,
+    divideScalar,
+    length,
+    multiplyScalar,
+  };
+
+  function copy(v: { x: number; y: number; z: number }) {
+    x = v.x;
+    y = v.y;
+    z = v.z;
+
+    return res;
+  }
+
+  function normalize() {
+    return divideScalar(length() || 1);
+  }
+
+  function divideScalar(scalar: number) {
+    return multiplyScalar(1 / scalar);
+  }
+
+  function length() {
+    return Math.sqrt(x * x + y * y + z * z);
+  }
+
+  function multiplyScalar(scalar: number) {
+    x *= scalar;
+    y *= scalar;
+    z *= scalar;
+
+    return res;
+  }
+
+  return res;
+}
+
+type BufferAttribute = {
+  array: ArrayBufferView;
+  usage: GLenum;
+  onUploadCallback?: () => void;
+  version?: number;
+  isFloat16BufferAttribute?: boolean;
+};
+
+type GlAttribute = {
+  buffer: WebGLBuffer;
+  type: number;
+  bytesPerElement: number;
+  version: number;
+  size: number;
+};
+
+function webGLAttributes(gl: WebGLRenderingContext) {
+  const buffers: WeakMap<BufferAttribute, GlAttribute> = new WeakMap();
+
+  function createBuffer(
+    attribute: BufferAttribute,
+    bufferType: GLenum
+  ): GlAttribute {
+    const array = attribute.array;
+    const usage: GLenum = attribute.usage;
+    const size = array.byteLength;
+
+    const buffer = gl.createBuffer()!;
+
+    gl.bindBuffer(bufferType, buffer);
+    gl.bufferData(bufferType, array, usage);
+
+    attribute.onUploadCallback?.();
+
+    let type: number;
+
+    if (array instanceof Float32Array) {
+      type = gl.FLOAT;
+    } else if (array instanceof Uint16Array) {
+      if (attribute.isFloat16BufferAttribute) {
+        type =
+          gl.getExtension("OES_texture_half_float")?.HALF_FLOAT_OES ||
+          gl.UNSIGNED_SHORT;
+      } else {
+        type = gl.UNSIGNED_SHORT;
+      }
+    } else if (array instanceof Int16Array) {
+      type = gl.SHORT;
+    } else if (array instanceof Uint32Array) {
+      type = gl.UNSIGNED_INT;
+    } else if (array instanceof Int32Array) {
+      type = gl.INT;
+    } else if (array instanceof Int8Array) {
+      type = gl.BYTE;
+    } else if (array instanceof Uint8Array) {
+      type = gl.UNSIGNED_BYTE;
+    } else if (array instanceof Uint8ClampedArray) {
+      type = gl.UNSIGNED_BYTE;
+    } else {
+      throw new Error("Unsupported buffer data format: " + array);
+    }
+
+    return {
+      buffer,
+      size,
+      type: type,
+      bytesPerElement: array.BYTES_PER_ELEMENT,
+      version: attribute.version || 0,
+    };
+  }
+
+  function updateBuffer(
+    buffer: WebGLBuffer,
+    attribute: BufferAttribute,
+    bufferType: number
+  ) {
+    const array = attribute.array;
+
+    gl.bindBuffer(bufferType, buffer);
+
+    gl.bufferSubData(bufferType, 0, array);
+    attribute.onUploadCallback?.();
+  }
+
+  function get(attribute: BufferAttribute) {
+    return buffers.get(attribute);
+  }
+
+  function remove(attribute: BufferAttribute): void {
+    const data = buffers.get(attribute);
+    if (data) {
+      gl.deleteBuffer(data.buffer);
+      buffers.delete(attribute);
+    }
+  }
+
+  function update(attribute: BufferAttribute, bufferType: number): WebGLBuffer {
+    const data = buffers.get(attribute);
+    if (data === undefined) {
+      const newData = createBuffer(attribute, bufferType);
+      buffers.set(attribute, newData);
+      return newData.buffer!;
+    }
+
+    if (data.version < attribute.version!) {
+      if (data.size !== attribute.array.byteLength) {
+        throw new Error(
+          "The size of the buffer attribute's array buffer does not match the original size. Resizing buffer attributes is not supported."
+        );
+      }
+      updateBuffer(data.buffer, attribute, bufferType);
+      data.version = attribute.version!;
+    }
+
+    return data.buffer;
+  }
+
+  return {
+    get,
+    remove,
+    update,
+  };
 }
